f27a6cf3ae019b463342bedafa0ab72ad6c3147f
umesh-yadav <yadavumesh8090@gmail.com>
2023-08-22 10:11:04 +0530

    changes done in account.js logic for negative balance

--- src/reducers/account.js
+++ src/reducers/account.js
@@ -11,7 +11,7 @@ export default function accountReducer(state={amount:2}, action){
         case incr:
             return {amount : state.amount + 1};
         case decr : 
-            if(state.amount<=0)
+            if(state.amount<0)
             {
                 alert("amount cannot be less than 0");
                 return {amount: state.amount, error: "amount cann't be less than 0"}
@@ -20,7 +20,7 @@ export default function accountReducer(state={amount:2}, action){
         case incrByAmt : 
             return {amount: state.amount + action.payload};
         case decrByAmt:
-            if(state.amount< action.payload){
+            if(state.amount - action.payload <0){
                 alert("amount cannot be negative. Please choose a different amount");
                 return {amount: state.amount, error: "amount cannot be negative"};
             }
